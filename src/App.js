fc3d6299fa5949e64a69f56935fdebab03cdc9d4
Maided Ponce <maided.h.p90@gmail.com>
2022-03-11 10:12:02 -0600

    adding project on repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,117 @@
 import logo from './logo.svg';
 import './App.css';
+import CardContainer from './components/cardContainer/CardContainer';
+import { useState } from 'react';
 
 function App() {
+  const [checked, setChecked] = useState({
+    checkedOne: false,
+    checkedTwo: false,
+    checkedThree: false,
+    checkedFour: false,
+    checkedFive: false,
+    checkedSixx: false
+  })
+
+  console.log(checked)
+
+  const users = [
+    {
+      nombre: "user apellido apellido",
+      tipo: "anfitrion",
+      estado: "activo",
+      validacion: "si",
+    },
+    {
+      nombre: "user apellido apellido",
+      tipo: "huesped",
+      estado: "activo",
+      validacion: "si",
+    },
+    {
+      nombre: "user apellido apellido",
+      tipo: "anfitrion",
+      estado: "inactivo",
+      validacion: "si",
+    },
+    {
+      nombre: "user apellido apellido",
+      tipo: "anfitrion",
+      estado: "inactivo",
+      validacion: "si",
+    },
+    {
+      nombre: "user apellido apellido",
+      tipo: "anfitrion",
+      estado: "activo",
+      validacion: "no",
+    }
+  ]
+
+  const admins = [
+    {
+      nombre: "user apellido apellido",
+      tipo: "anfitrion",
+      estado: "activo",
+      validacion: "si",
+    },
+    {
+      nombre: "user apellido apellido",
+      tipo: "huesped",
+      estado: "activo",
+      validacion: "si",
+    },
+    {
+      nombre: "user apellido apellido",
+      tipo: "anfitrion",
+      estado: "inactivo",
+      validacion: "si",
+    },
+    {
+      nombre: "user apellido apellido",
+      tipo: "anfitrion",
+      estado: "inactivo",
+      validacion: "si",
+    },
+    {
+      nombre: "user apellido apellido",
+      tipo: "anfitrion",
+      estado: "activo",
+      validacion: "no",
+    }
+  ]
+
+  const filterUsers = users.filter((u) => checked.checkedOne && u.tipo === "huesped" || checked.checkedTwo && u.tipo === "anfitrion" || checked.checkedThree && u.estado === "activo" || checked.checkedFour && u.estado === "inactivo" || checked.checkedFive && u.validacion === "si" || checked.checkedSixx && u.validacion === "no")
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>holi</h1>
+      <div className="containerCards">
+        <CardContainer setChecked={setChecked} checked={checked} checkedOne={checked.checkedOne} checkedTwo={checked.checkedTwo} type={"Tipo"} />
+        <CardContainer setChecked={setChecked} checked={checked} checkedThree={checked.checkedThree} checkedFour={checked.checkedFour} type={"Estado"} />
+        <CardContainer setChecked={setChecked} checked={checked} checkedFive={checked.checkedFive} checkedSixx={checked.checkedSixx} type={"Validación"} />
+      </div>
+      <div className="containerUsers">
+        { 
+          filterUsers.length === 0 ? (
+            users.map((u) => (
+            <article>
+            <p>{u.nombre}</p>
+            <p>{u.tipo}</p>
+            <p>{u.estado}</p>
+            <p>{u.validacion}</p>
+          </article>))
+          ) : (
+            filterUsers.map((u) => (
+              <article>
+              <p>{u.nombre}</p>
+              <p>{u.tipo}</p>
+              <p>{u.estado}</p>
+              <p>{u.validacion}</p>
+            </article>
+          )))
+        }
+      </div>
     </div>
   );
 }
